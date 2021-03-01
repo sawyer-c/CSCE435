a809d463e225f30030fdccb12ab72eafaef03eac
sawyer-c <sawyer_cowan818@yahoo.com>
2021-03-01 16:17:09 -0600

    bash test

---
+++ HW2/List_minimum/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./list_minimum.exe 200000000 1
+./list_minimum.exe 200000000 2
+./list_minimum.exe 200000000 4
+./list_minimum.exe 200000000 8
+./list_minimum.exe 200000000 16
+./list_minimum.exe 200000000 32
+./list_minimum.exe 200000000 64
+./list_minimum.exe 200000000 128
+./list_minimum.exe 200000000 256
+./list_minimum.exe 200000000 512
+./list_minimum.exe 200000000 1024
+./list_minimum.exe 200000000 2048
+./list_minimum.exe 200000000 4096
+./list_minimum.exe 200000000 8192
