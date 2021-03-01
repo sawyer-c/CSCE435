e13c912cb89567d51d1a58aa0fe2fdf256b99139
sawyer-c <sawyer_cowan818@yahoo.com>
2021-03-01 16:25:23 -0600

    add run script

---
+++ HW2/Barrier/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./barrier.exe 2
+./barrier.exe 4
+./barrier.exe 8
+./barrier.exe 16
+./barrier.exe 32
+./barrier.exe 64
+./barrier.exe 128
+./barrier.exe 256
+./barrier.exe 512
+./barrier.exe 1024
+./barrier.exe 2048
+./barrier.exe 4096
+./barrier.exe 8192
+./barrier.exe 16384
