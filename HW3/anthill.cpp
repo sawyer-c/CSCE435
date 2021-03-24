a85491ae88ba27a33ea11a2ecb3607f7a2d179aa
sawyer-c <sawyer_cowan818@yahoo.com>
2021-03-23 23:24:36 -0500

    division and modulo

--- HW3/anthill.cpp
+++ HW3/anthill.cpp
@@ -264,14 +264,24 @@ int main (int argc, char **argv) {
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Example Approach #1: Brute force approach
+    
+    //calculate i value start
+    // omp_get_thread_num()
+
+    //calculate j value start
+    
+    
+    //set that for each thread
+
+
 
     start_time = omp_get_wtime(); 
     volatile int found = 0;
 #pragma omp parallel for default(none) shared(MyLawn, found)
-    for (int i = 0 + omp_get_thread_num(); i < MyLawn.m; i++) {
+    for (int i = (omp_get_thread_num()*(size*size/omp_get_num_threads()) % size; i < MyLawn.m; i++) {
         // int thread_num = omp_get_thread_num();
         // printf("Thread Num = %d", thread_num);
-	for (int j = 0 + omp_get_thread_num(); j < MyLawn.m; j++) {
+	for (int j = (omp_get_thread_num()*(size*size/omp_get_num_threads()) / size; j < MyLawn.m; j++) {
 	    if (found == 0) {
 		if (MyLawn.number_of_ants_in_cell(i,j) >= 1) {
             if (MyLawn.guess_anthill_location(i,j) == 1)
