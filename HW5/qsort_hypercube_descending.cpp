c70726a90727ebe5e5660657c5c14d798291bc2b
sawyer-c <sawyer_cowan818@yahoo.com>
2021-04-15 23:34:38 -0500

    fix

--- HW5/qsort_hypercube_descending.cpp
+++ HW5/qsort_hypercube_descending.cpp
@@ -316,43 +316,11 @@ void HyperCube_Class::HyperCube_QuickSort() {
 	if (nbr_k > my_id) {
 		//add modulo to ensure nbr_k value is within the hypercube size 
 		nbr_k = nbr_k % sub_hypercube_size;
-	    // MPI-6: Receive number of elements greater than pivot
-	    // ***** Add MPI call here *****
-		MPI_Recv(&nbr_list_size, 1, MPI_INT, nbr_k, 0, sub_hypercube_comm, MPI_STATUS_IGNORE);
-
-	    // MPI-7: Send number of elements less than or equal to pivot
-	    // ***** Add MPI call here *****
-		MPI_Send(&list_size_leq, 1, MPI_INT, nbr_k, 0, sub_hypercube_comm);
-
-	    // Allocate storage for neighbor's list
-	    nbr_list = new int[nbr_list_size]; 
-
-	    // MPI-8: Receive neighbor's list of elements that are greater than the pivot
-	    // ***** Add MPI call here *****
-		MPI_Recv(nbr_list, nbr_list_size, MPI_INT, nbr_k, 0, sub_hypercube_comm, MPI_STATUS_IGNORE);
-
-	    // MPI-9: Send list[0 ... idx-1] to neighbor
-	    // ***** Add MPI call here *****
-		MPI_Send(list, list_size_leq, MPI_INT, nbr_k, 0, sub_hypercube_comm);
-
-	    // Merge local list of elements greater than pivot with neighbor's list
-	    new_list = merged_list(&list[idx], list_size_gt, nbr_list, nbr_list_size); 
-
-	    // Replace local list with new_list, update size
-	    delete [] list; 
-	    delete [] nbr_list; 
-	    list = new_list; 
-	    list_size = list_size_gt+nbr_list_size;
-
-	} else {
-		
-		//add modulo to ensure nbr_k value is within the hypercube size 
-		nbr_k = nbr_k % sub_hypercube_size;
 
 	    // MPI-2: Send number of elements greater than pivot
 
 	    // ***** Add MPI call here *****
-		MPI_Send(&list_size_gt, 1, MPI_INT, nbr_k, 0, sub_hypercube_comm);
+		MPI_Send(&list_size_leq, 1, MPI_INT, nbr_k, 0, sub_hypercube_comm);
 
 	    // MPI-3: Receive number of elements less than or equal to pivot
 		// ***** Add MPI call here *****
@@ -364,7 +332,7 @@ void HyperCube_Class::HyperCube_QuickSort() {
 
 	    // MPI-4: Send list[idx ... list_size-1] to neighbor
 		// ***** Add MPI call here *****
-		MPI_Send(&list[idx], list_size_gt, MPI_INT, nbr_k, 0, sub_hypercube_comm);
+		MPI_Send(&list[idx], list_size_leq, MPI_INT, nbr_k, 0, sub_hypercube_comm);
 	    
 
 
@@ -382,6 +350,37 @@ void HyperCube_Class::HyperCube_QuickSort() {
 	    delete [] nbr_list; 
 	    list = new_list; 
 	    list_size = list_size_leq+nbr_list_size;
+
+	} else {
+		//add modulo to ensure nbr_k value is within the hypercube size 
+		nbr_k = nbr_k % sub_hypercube_size;
+	    // MPI-6: Receive number of elements greater than pivot
+	    // ***** Add MPI call here *****
+		MPI_Recv(&nbr_list_size, 1, MPI_INT, nbr_k, 0, sub_hypercube_comm, MPI_STATUS_IGNORE);
+
+	    // MPI-7: Send number of elements less than or equal to pivot
+	    // ***** Add MPI call here *****
+		MPI_Send(&list_size_gt, 1, MPI_INT, nbr_k, 0, sub_hypercube_comm);
+
+	    // Allocate storage for neighbor's list
+	    nbr_list = new int[nbr_list_size]; 
+
+	    // MPI-8: Receive neighbor's list of elements that are greater than the pivot
+	    // ***** Add MPI call here *****
+		MPI_Recv(nbr_list, nbr_list_size, MPI_INT, nbr_k, 0, sub_hypercube_comm, MPI_STATUS_IGNORE);
+
+	    // MPI-9: Send list[0 ... idx-1] to neighbor
+	    // ***** Add MPI call here *****
+		MPI_Send(list, list_size_gt, MPI_INT, nbr_k, 0, sub_hypercube_comm);
+
+	    // Merge local list of elements greater than pivot with neighbor's list
+	    new_list = merged_list(&list[idx], list_size_gt, nbr_list, nbr_list_size); 
+
+	    // Replace local list with new_list, update size
+	    delete [] list; 
+	    delete [] nbr_list; 
+	    list = new_list; 
+	    list_size = list_size_gt+nbr_list_size;
 	}
 	// Deallocate processor group, processor communicator, 
 	// sub_hypercube_processors array; these variables will be 
